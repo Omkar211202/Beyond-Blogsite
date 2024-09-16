5727d4db24594bb2414e83cbde2115c468dcf2d9
Omkar Kathi <144525935+Omkar211202@users.noreply.github.com>
2024-09-16 10:14:58 +0000

    google meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -90,6 +90,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         href={`${basePath}/static/favicons/safari-pinned-tab.svg`}
         color="#5bbad5"
       />
+      <meta name="google-site-verification" content="_38yBmP9CBggXItniVSR9Y69cPGoU415bvyvR9vCc54" />
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
