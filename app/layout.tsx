978f0ad4640b354c2078c96e9b789f546c4edb76
Omkar Kathi <144525935+Omkar211202@users.noreply.github.com>
2024-09-30 12:25:47 +0000

    og changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export const metadata: Metadata = {
     description: siteMetadata.description,
     url: './',
     siteName: siteMetadata.title,
-    images: [siteMetadata.socialBanner],
+    images: siteMetadata.socialBanner,
     locale: 'en_US',
     type: 'website',
   },
