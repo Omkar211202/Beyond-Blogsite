3a3074d40994922994aa912764f65398782403ba
Omkar Kathi <144525935+Omkar211202@users.noreply.github.com>
2025-01-25 10:03:49 +0000

    commit

--- app/sw.js
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line no-undef
-importScripts('https://cdn.brevo.com/js/sdk-loader.js')
-// eslint-disable-next-line no-undef
-Brevo.push([
-  'init',
-  {
-    client_key: (location.search.match(/[?&]key=([^&]*)/) || [])[1],
-  },
-])
