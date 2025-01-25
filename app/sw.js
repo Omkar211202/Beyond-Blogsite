5a4563d3bca86e3f404d0e2d88e94fc05ba42535
Omkar Kathi <144525935+Omkar211202@users.noreply.github.com>
2025-01-25 10:49:46 +0000

    once again check

---
+++ app/sw.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-undef */
+importScripts('https://cdn.brevo.com/js/sdk-loader.js')
+Brevo.push([
+  'init',
+  {
+    client_key: (location.search.match(/[?&]key=([^&]*)/) || [])[1],
+  },
+])
